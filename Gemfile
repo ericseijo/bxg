8f4364378cfbb41ed3ab72e672f01d501a278647
Eric Seijo <ericseijo@gmail.com>
2012-10-21 11:38:38 -0400

    initial commit

---
+++ Gemfile
@@ -0,0 +1,28 @@
+source 'https://rubygems.org'
+
+gem 'rails', '3.2.8'
+
+
+group :development, :test do
+	gem 'rspec-rails'
+end
+
+# Gems used only for assets and not required
+# in production environments by default.
+group :assets do
+  gem 'sass-rails',   '~> 3.2.3'
+  gem 'coffee-rails', '~> 3.2.1'
+  gem 'uglifier', '>= 1.0.3'
+end
+
+group :test do
+	gem 'capybara'
+  gem 'factory_girl_rails'
+  gem 'database_cleaner'
+end
+
+
+gem 'jquery-rails'
+gem 'pg'
+
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bxg::Application.config.secret_token = 'f00ba0a8cda9b5164b963e8aeb94037aabef6996695ea6b1f8b1e5a0775920b83bb295269dab9b9c286ce170ba97d8d04d5d6a84f15c5e16927fcab3aa67c7db'
